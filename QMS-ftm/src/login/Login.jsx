8fa01bfeb38e7be8d1ddf6fb952adb31133a5c41
farha_thasni_m <farhathasnim03@gmail.com>
2024-05-04 21:25:03 +0530

    edit

--- QMS-ftm/src/login/Login.jsx
+++ QMS-ftm/src/login/Login.jsx
@@ -1,64 +1,72 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { signIn } from '../firebase';
-import { Card, CardHeader, CardBody, Input, Button } from '@nextui-org/react';
-import Navbar from './Navbar';
+import { useState } from "react";
+import Navbar from "./Navbar";
+import { signIn } from "../firebase";
+import { Card, CardHeader, CardBody, Input, Button } from "@nextui-org/react";
+// import AdminDash from "../Admin/AdminDash"
+// import CounterDash from "../Counter/CounterDash"
+import { useNavigate } from "react-router-dom";
 
-const Login = ({ onLogin }) => {
+const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState(null);
-  const navigate = useNavigate(); // To navigate to different routes after login
+  const [loggedInAs, setLoggedInAs] = useState(null);
+  const navigate = useNavigate();
 
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
       const role = await signIn(email, password);
-      if (role === 'admin') {
-        onLogin(role); // Update the state
-        navigate('/admin'); // Redirect to admin dashboard
-      } else if (role === 'counter') {
-        onLogin(role); // Update the state
-        navigate('/counter'); // Redirect to counter dashboard
+      if (role === "admin") {
+        setLoggedInAs("admin");
+      } else if (role === "counter") {
+        setLoggedInAs("counter");
       } else {
-        setError('Unauthorized access');
+        setError("Unauthorized access");
       }
     } catch (error) {
       setError(error.message);
     }
   };
 
+  if (loggedInAs === "admin") {
+    navigate("/adminDash");
+  } else if (loggedInAs === "counter") {
+    navigate("/counterDash");
+  }
+
   return (
-    <div className="md:mx-64 mx-2 md:py-10 py-5 flex flex-col min-h-dvh min-w-screen">
+    <div className=" flex flex-col min-h-dvh min-w-screen">
       <Navbar />
-      <div className="flex flex-1 items-center justify-center">
-        <Card className="p-5 md:min-w-96">
+      <div className="md:mx-64 mx-2 md:py-10 py-5 flex flex-1 items-center justify-center">
+        <Card className="p-2 md:p-5 md:min-w-96 min-w-[75vw]">
           <CardHeader className="justify-center">
             <h2 className="font-semibold">Please Login Here</h2>
           </CardHeader>
-          <form onSubmit={handleLogin}>
+          <form action="post" onSubmit={handleLogin}>
             <CardBody className="gap-5">
               <Input
                 type="email"
                 label="Email"
                 value={email}
-                autoComplete="off"
+                autoComplete='off'
                 onChange={(e) => setEmail(e.target.value)}
                 required
+                variant='bordered'
               />
               <Input
                 type="password"
                 label="Password"
                 value={password}
+                id="password"
                 onChange={(e) => setPassword(e.target.value)}
                 required
+                variant='bordered'
               />
               {error && <p className="text-red-500 text-xs italic">{error}</p>}
-              <Button className="bg-[#6236F5] text-white" type="submit">
-                Submit
-              </Button>
+              <Button className="bg-[#6236F5] text-white" type="submit">Submit</Button>
             </CardBody>
-          </form>
+          </form >
         </Card>
       </div>
     </div>
